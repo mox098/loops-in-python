bccbfc3edeeee2821cf0e5edebc4f5c7df159333
mox098 <moxasangani1@gmail.com>
2026-04-22 21:18:41 +0100

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,7 +1,10 @@
 number = int(input("Enter the number: "))
 n = int(input("Enter the power: "))
 
+result=1
 
-result = number ** n
+for i in range(n):
+    result=result*number
+
 
 print("The result is:", result)
